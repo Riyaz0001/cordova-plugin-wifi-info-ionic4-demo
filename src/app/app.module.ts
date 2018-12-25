45a098ea28a661295595dde304a432f9674709fb
MD.Riyaz <m.r.u.khan2017@gmail.com>
2018-12-25 18:57:37 +0600

    Example init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// import { NetworkInterface } from '@ionic-native/network-interface/ngx';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        // NetworkInterface
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+declare var wifiinformation: any;
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    constructor(private loadingCtrl: LoadingController) { }
+
+
+
+
+    getWifiIPAddress() {
+        wifiinformation.getWifiInfo(success => {
+            alert('Success: ' + JSON.stringify(success));
+
+        }, (err) => console.error(err));
+    }
+
+    async getActiveDevices() {
+        const loading = await this.loadingCtrl.create({
+            spinner: 'crescent',
+            message: 'Please Wait...'
+        });
+        await loading.present();
+
+        // get all active devices
+        wifiinformation.getActiveDevices(success => {
+            loading.dismiss();
+            alert('Success: ' + JSON.stringify(success));
+
+        }, (err) => {
+            loading.dismiss();
+            console.error(err);
+        });
+    }
+
+    getDHCPInfo() {
+        wifiinformation.getDHCPInfo(success => {
+            alert('Success: ' + JSON.stringify(success));
+
+        }, (err) => console.error(err));
+    }
+
+    getSampleInfo() {
+        wifiinformation.getSampleInfo(wifi => {
+            alert(
+                'SSID: ' + wifi.ssid +
+                '\nMAC: ' + wifi.mac +
+                '\nIP: ' + wifi.ip +
+                '\nGateway: ' + wifi.gateway
+                );
+
+        }, (err) => console.error(err));
+    }
+
 
 }
